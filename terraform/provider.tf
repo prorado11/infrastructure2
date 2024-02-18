e511a82b12cde7209bf29f671bad228f93918f55
Rado <prado11@gmail.com>
2024-02-18 10:52:58 -0600

    providers upgrade

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.3.3"
+  required_version = "~> 1.5.5"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.58.0"
+      version = "5.4.0"
     }
     helm = {
         source  = "hashicorp/helm"
